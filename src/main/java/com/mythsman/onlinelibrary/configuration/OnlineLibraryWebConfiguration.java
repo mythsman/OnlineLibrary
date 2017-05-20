2e913494168513757cf0d433047f53937ced4e36
mythsman <dqz48548263@qq.com>
2017-05-20 15:21:43 +0800

    Add wechat access_token

--- src/main/java/com/mythsman/onlinelibrary/configuration/OnlineLibraryWebConfiguration.java
+++ src/main/java/com/mythsman/onlinelibrary/configuration/OnlineLibraryWebConfiguration.java
@@ -4,7 +4,6 @@ import com.mythsman.onlinelibrary.interceptor.PassportInterceptor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
 import org.springframework.web.servlet.config.annotation.InterceptorRegistry;
-import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
 
 /**
