c2b5cd0e075ce5e125989626f9bd1e058ea93a09
mythsman <dqz48548263@qq.com>
2017-05-24 14:02:15 +0800

    First commit

--- src/main/java/com/mythsman/onlinelibrary/controller/AppController.java
+++ src/main/java/com/mythsman/onlinelibrary/controller/AppController.java
@@ -4,10 +4,12 @@ import com.alibaba.fastjson.JSON;
 import com.mythsman.onlinelibrary.dao.ArticleDao;
 import com.mythsman.onlinelibrary.model.Article;
 import com.mythsman.onlinelibrary.service.CourseService;
+import org.apache.ibatis.annotations.Param;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.*;
+import org.springframework.web.multipart.MultipartFile;
 
 import java.util.List;
 import java.util.Map;
@@ -66,6 +68,12 @@ public class AppController {
         Map<String,Map<String,List<String>>> schools= courseService.getCourseMap();
         model.addAttribute("courseMap",schools);
         return "upload";
+    }
+
+    @RequestMapping(path = {"/upload"}, method = {RequestMethod.POST},params = {"school","college","course","file"})
+    public String getFile(@Param("school")String school, @Param("college")String college, @Param("course")String course, @Param("file")MultipartFile multipartFile) {
+
+        return "redirect:/app/tab3";
     }
 
     @RequestMapping(path = {"/about"}, method = {RequestMethod.GET})
